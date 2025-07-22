29b6aee9d948f979d17a525fd3a83e1adccd68d9
Jerry Guerrero <JerryGDev126@gmail.com>
2025-07-21 21:19:18 -0700

    more progress on learning love, finished chapter 6

--- main.lua
+++ main.lua
@@ -1,7 +1,36 @@
-name = "Jerry"
-age = "28"
-text = "Hello, my name is " .. name .. ", and I'm "..age.." years old."
-print(text)
+function love.load()
+    x = 100
+    y = 50
+    move = true
+end
+
+function love.draw()
+    love.graphics.rectangle("line", x, y, 200, 150)
+end
+
+function love.update(dt)
+    if love.keyboard.isDown("right") then
+        print("moving right!")
+        x = x + 100 * dt
+    elseif love.keyboard.isDown("left") then
+        print("moving left!")
+        x = x - 100 * dt
+    elseif love.keyboard.isDown("up") then
+        print("moving up!")
+        y = y - 100 * dt
+    elseif love.keyboard.isDown("down") then
+        print("moving down!")
+        y = y + 100 * dt
+    end
+end
+
+if 5 < 9 and 14 > 7 then
+    print("Both statements are true")
+end
+
+if 20 < 9 or 14 > 7 or 5 == 10 then
+    print("One of these staements is true")
+end
 
 if arg[2] == "debug" then
     require("lldebugger").start()
