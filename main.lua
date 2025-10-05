9d05784e347ca1cac696d674343bd119a69bbd0d
Jerry Guerrero <JerryGDev126@gmail.com>
2025-10-05 04:33:05 -0700

    More work, learned about angles and distance

--- main.lua
+++ main.lua
@@ -1,29 +1,28 @@
 function love.load()
-    circle = {}
-
-    circle.x = 100
-    circle.y = 100
-    circle.radius = 25
-    circle.speed = 200
+    arrow = {}
+    arrow.x = 200
+    arrow.y = 200
+    arrow.speed = 300
+    arrow.angle = 0
+    arrow.image = love.graphics.newImage("arrow_right.png")
+    arrow.origin_x = arrow.image:getWidth() / 2
+    arrow.origin_y = arrow.image:getHeight() / 2
 end
 
 function love.update(dt)
     mouse_x, mouse_y = love.mouse.getPosition()
+    arrow.angle = math.atan2(mouse_y - arrow.y, mouse_x - arrow.x)
+    cos = math.cos(arrow.angle)
+    sin = math.sin(arrow.angle)
 
-    angle = math.atan2(mouse_y - circle.y, mouse_x - circle.x)
-
-    cos = math.cos(angle)
-    sin = math.sin(angle)
-
-    circle.x = circle.x + circle.speed * cos * dt
-    circle.y = circle.y + circle.speed * sin * dt
+    
+    arrow.x = arrow.x + arrow.speed * cos * dt
+    arrow.y = arrow.y + arrow.speed * sin * dt
 end
 
 function love.draw()
-    love.graphics.circle("line", circle.x, circle.y, circle.radius)
-    love.graphics.line(circle.x, circle.y, mouse_x, mouse_y)
-    love.graphics.line(circle.x, circle.y, mouse_x, circle.y)
-    love.graphics.line(mouse_x, mouse_y, mouse_x, circle.y)
+    love.graphics.draw(arrow.image, arrow.x, arrow.y, arrow.angle, 1, 1, arrow.origin_x, arrow.origin_y)
+    love.graphics.circle("fill", mouse_x, mouse_y, 5)
 end
 
 function getDistance(x1, y1, x2, y2)
@@ -32,6 +31,10 @@ function getDistance(x1, y1, x2, y2)
 
     local a = horizontal_distance ^2
     local b = vertical_distance ^2
+
+    local c = a + b
+    local distance = math.sqrt(c)
+    return distance
 end
 
 if arg[2] == "debug" then
