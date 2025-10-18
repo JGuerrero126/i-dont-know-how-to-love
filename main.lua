e5e7bcd7b25423c3ff0d0e08f35c1a908983a4db
Jerry Guerrero <JerryGDev126@gmail.com>
2025-10-17 17:44:02 -0700

    finished next chapter, feeling good

--- main.lua
+++ main.lua
@@ -1,40 +1,38 @@
 function love.load()
-    arrow = {}
-    arrow.x = 200
-    arrow.y = 200
-    arrow.speed = 300
-    arrow.angle = 0
-    arrow.image = love.graphics.newImage("arrow_right.png")
-    arrow.origin_x = arrow.image:getWidth() / 2
-    arrow.origin_y = arrow.image:getHeight() / 2
+    image = love.graphics.newImage("jump_3.png")
+
+    local width = image:getWidth()
+    local height = image:getHeight()
+
+    frames = {}
+
+    local frame_width = 117
+    local frame_height = 233
+
+    maxFrames = 5
+    for i=0,1 do
+        for j=0,2 do
+            table.insert(frames, love.graphics.newQuad(j * frame_width, i * frame_height, frame_width, frame_height, width, height))
+            if #frames == maxFrames then
+                break
+            end
+        end
+        print("I don't break!")
+    end
+    currentFrame = 1
 end
 
 function love.update(dt)
-    mouse_x, mouse_y = love.mouse.getPosition()
-    arrow.angle = math.atan2(mouse_y - arrow.y, mouse_x - arrow.x)
-    cos = math.cos(arrow.angle)
-    sin = math.sin(arrow.angle)
+   currentFrame = currentFrame + 10 * dt
 
-    
-    arrow.x = arrow.x + arrow.speed * cos * dt
-    arrow.y = arrow.y + arrow.speed * sin * dt
+   if currentFrame >= 6 then
+    currentFrame = 1
+   end
 end
 
 function love.draw()
-    love.graphics.draw(arrow.image, arrow.x, arrow.y, arrow.angle, 1, 1, arrow.origin_x, arrow.origin_y)
-    love.graphics.circle("fill", mouse_x, mouse_y, 5)
-end
-
-function getDistance(x1, y1, x2, y2)
-    local horizontal_distance = x1 - x2
-    local vertical_distance = y1 - y2
-
-    local a = horizontal_distance ^2
-    local b = vertical_distance ^2
-
-    local c = a + b
-    local distance = math.sqrt(c)
-    return distance
+    -- love.graphics.draw(image, frames[1], 100, 100)
+   love.graphics.draw(image, frames[math.floor(currentFrame)], 100, 100)
 end
 
 if arg[2] == "debug" then
