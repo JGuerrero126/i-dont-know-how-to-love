4b4529711c083b7df60089253fc9b909962b302c
Jerry Guerrero <JerryGDev126@gmail.com>
2025-10-26 18:39:59 -0700

    More progress, learned about the beginning of tiles

--- main.lua
+++ main.lua
@@ -1,38 +1,34 @@
 function love.load()
-    image = love.graphics.newImage("jump_3.png")
+   tilemap = {
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 2, 2, 2, 2, 2, 2, 2, 2, 1},
+    {1, 2, 3, 4, 5, 5, 4, 3, 2, 1}, 
+    {1, 2, 2, 2, 2, 2, 2, 2, 2, 1}, 
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+    }
 
-    local width = image:getWidth()
-    local height = image:getHeight()
-
-    frames = {}
-
-    local frame_width = 117
-    local frame_height = 233
-
-    maxFrames = 5
-    for i=0,1 do
-        for j=0,2 do
-            table.insert(frames, love.graphics.newQuad(j * frame_width, i * frame_height, frame_width, frame_height, width, height))
-            if #frames == maxFrames then
-                break
-            end
-        end
-        print("I don't break!")
-    end
-    currentFrame = 1
+    colors = {
+        {1, 1, 1},
+        {1, 0, 0},
+        {1, 0, 1},
+        {0, 0, 1},
+        {0, 1, 1}
+    }
 end
 
 function love.update(dt)
-   currentFrame = currentFrame + 10 * dt
-
-   if currentFrame >= 6 then
-    currentFrame = 1
-   end
+ 
 end
 
 function love.draw()
-    -- love.graphics.draw(image, frames[1], 100, 100)
-   love.graphics.draw(image, frames[math.floor(currentFrame)], 100, 100)
+    for i,row in ipairs(tilemap) do
+        for j,tile in ipairs(row) do
+            if tile ~= 0 then
+                love.graphics.setColor(colors[tile])
+                love.graphics.rectangle("fill", j * 25, i * 25, 25, 25)
+            end
+        end
+    end
 end
 
 if arg[2] == "debug" then
